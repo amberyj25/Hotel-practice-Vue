105b7a4a5dd93ca20f5da277180cfd01236bcc47
amber <amber20240@gmail.com>
2020-07-26 19:36:37 +0800

    新增rooms hover div資訊區域向上延伸

--- src/components/Rooms.vue
+++ src/components/Rooms.vue
@@ -3,8 +3,13 @@
      <a href="##" @click.prevent="updateUrl(item.id)" class="li_img">
         <img :src="item.imageUrl">
       </a>
-      <a href="##" @click.prevent="updateUrl(item.id)" class="li_text">{{item.name}}</a>
-      <p>平日：{{item.normalDayPrice}}元 / 假日：{{item.holidayPrice}}元</p>
+      <div class="li_inner">
+        <a href="##" @click.prevent="updateUrl(item.id)" class="li_text">{{item.name}}</a>
+        <p>平日：{{item.normalDayPrice}}元 / 假日：{{item.holidayPrice}}元</p>
+        <div class="more">
+          <a href="##" @click.prevent="updateUrl(item.id)">點擊看更多介紹</a>
+        </div>
+      </div>
     </li>
 </template>
 
@@ -25,10 +30,41 @@ li{
   margin: 12px;
   background-color: #fffffc;
   padding: 15px;
+  position: relative;
+  height: 320px;
+  overflow: hidden;
 }
 li img{
   width: 350px;
   height: 250px;
+}
+li .li_inner{
+  background-color: white;
+  position: absolute;
+  bottom:0;
+  width: 350px;
+  height: 80px;
+}
+li .li_inner:hover{
+  height: 120px;
+}
+li .li_inner .more{
+  position:absolute;
+  width:100%;
+  height: 20px;
+  bottom: -21px;
+} 
+li .li_inner:hover .more{
+  bottom: 15px;
+}
+li .li_inner .more a{
+  text-decoration: none;
+  color: #0033FF;
+  font-size: 18px;
+  letter-spacing: 5px;
+}
+li .li_inner .more a:hover{
+  color: #0000CD;
 }
 li .li_text{
   display: block;
